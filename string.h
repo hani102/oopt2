26f4934850479069c7ea853e694e9457bb9a6de4
hani102 <117590169+hani102@users.noreply.github.com>
2022-12-07 10:21:03 -0800

    Update string.h

--- string.h
+++ string.h
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class String {
+private:
+	char* a;
+	char* b;
+	char l;
+	char x;
+	int z;
+	string j;
+	int h;
+	int v;
+
+public:
+	String();
+	String(const String& str);
+	String(const String& str, int pos, int len);
+	String(const char* s);
+	String(const char* s, int n);
+	String(int n, char c);
+	int length();
+	char at(int i);
+	String substr(int pos, int len) ;
+	friend ostream& operator<< (ostream& os, const String& str);
+
+
+
+
+};
 

1361232dce10ac597c5785fe2bf46171e1922ec9
hani102 <117590169+hani102@users.noreply.github.com>
2022-12-07 10:21:54 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include "String.h"
 
+int main() {
+
+	String c1;
+	String obj1;
+	String c2;
+	String c3;
+	String c4;
+	String c5;
+	String c6;
+	cout << obj1.length();
+	String c7;
+	String c8;
+
+
+
+}
